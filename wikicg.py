638e8f9806f59ead6073e30f0518642bb218ec7c
mr.Shu <mr@shu.io>
2015-11-22 23:43:49 +0100

    wikicg: Reorganized and fixed
    * Reorganized wikicg in such a way so that it makes way more sense.
    
    * Fixed a few bugs in the process.
    
    Signed-off-by: mr.Shu <mr@shu.io>

--- wikicg.py
+++ wikicg.py
@@ -7,7 +7,8 @@ import Queue
 
 
 def clean_title(title):
-    return title.encode('ascii', 'ignore').replace('/', '_').replace(' ', '_')
+    return title.encode('ascii', 'ignore').lower()\
+        .replace('/', '_').replace(' ', '_')
 
 
 def dump_page(lang, limit, visited, q, path):
@@ -15,32 +16,38 @@ def dump_page(lang, limit, visited, q, path):
         return
 
     if q.empty():
+        print("Queue is empty")
         return
 
     title = q.get()
     if title in visited:
-        return
+        return dump_page(lang, limit, visited, q, path)
 
     page = None
+    wikipedia.set_lang(lang)
+    try:
+        page = wikipedia.page(title)
+    except:
+        page = None
 
-    with open('{}{}/{}.txt'.format(path, lang, clean_title(title)), 'w+') as f:
-        wikipedia.set_lang(lang)
-        try:
-            page = wikipedia.page(title)
-        except:
-            page = None
+    if page is not None:
+        real_path = '{}/{}/{}.txt'.format(path, lang, clean_title(title))
+        summary = page.summary.encode('utf-8').replace('\n', ' ')
+        if len(summary) < 1:
+            print("Empty summary, skipping ({}) {}".format(limit, page))
+        else:
+            with open(real_path, 'w+') as f:
+                f.write(summary + '\n')
+                print("Dumped ({}) {}".format(limit, page))
+                limit -= 1
 
-        if page is not None:
-            f.write(page.summary.encode('utf-8'))
-            print("Dumped ({}) {}".format(limit, page))
-
-        visited.append(title)
+    visited.append(title)
 
     if hasattr(page, 'links'):
         for link in page.links:
             q.put(link)
 
-    dump_page(lang, limit - 1, visited, q, path)
+    dump_page(lang, limit, visited, q, path)
 
 
 @click.command()
